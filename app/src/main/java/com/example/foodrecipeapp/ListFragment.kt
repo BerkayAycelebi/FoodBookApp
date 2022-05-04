bd2d7653c3f6eec023a5fc227e890b4921e7de16
Rıza Berkay AYÇELEBİ <differentiate.function@gmail.com>
2022-05-04 13:53:34 +0300

    Recycleview ayarları yapıldı

--- app/src/main/java/com/example/foodrecipeapp/ListFragment.kt
+++ app/src/main/java/com/example/foodrecipeapp/ListFragment.kt
@@ -1,14 +1,20 @@
 package com.example.foodrecipeapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_list.*
 
 
 class ListFragment : Fragment() {
-
+  var foodNameList=ArrayList<String>()
+    var foodIdList =ArrayList<Int>()
+    private lateinit var listAdapter:ListRecycleAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,6 +26,41 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+       listAdapter= ListRecycleAdapter(foodNameList,foodIdList)
+        recyclerView.layoutManager=LinearLayoutManager(context)
+        recyclerView.adapter=listAdapter
+        getDatas()
+    }
+    fun getDatas()
+    {
+        try{
+            activity?.let {
+                val database=it.openOrCreateDatabase("Foods",Context.MODE_PRIVATE,null)
+                val cursor=database.rawQuery("SELECT * FROM foods",null)
+                val foodNameIndex=cursor.getColumnIndex("foodName")
+                val foodIdIndex=cursor.getColumnIndex("id")
+                    foodNameList.clear()
+                    foodIdList.clear()
+                while(cursor.moveToNext())
+                {
+                    foodNameList.add(cursor.getString(foodNameIndex))
+                    foodIdList.add(cursor.getInt(foodIdIndex))
+
+                }
+                listAdapter.notifyDataSetChanged()
+                cursor.close()
+            }
+
+        }
+        catch (e:Exception)
+        {
+            e.printStackTrace()
+        }
     }
 
 
---
+++ app/src/main/java/com/example/foodrecipeapp/ListRecycleAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.foodrecipeapp
+
+import android.view.LayoutInflater
+import androidx.appcompat.view.menu.ActionMenuItemView
+import  android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.recycle_row.view.*
+
+class ListRecycleAdapter(val foodlist:ArrayList<String>, val idlist:ArrayList<Int>):RecyclerView.Adapter<ListRecycleAdapter.FoodHolder>() {
+
+    class FoodHolder(itemView: View):RecyclerView.ViewHolder(itemView)
+    {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodHolder {
+      val inflater=LayoutInflater.from(parent.context)
+        val view=inflater.inflate(R.layout.recycle_row,parent,false)
+    return FoodHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: FoodHolder, position: Int) {
+     holder.itemView.recyle_row_text.text=foodlist[position]
+    }
+
+    override fun getItemCount(): Int {
+        return foodlist.size
+    }
+}
